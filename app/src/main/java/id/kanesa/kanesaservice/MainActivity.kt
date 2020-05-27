ab04e3967633dc18a066a9be27ca839a4767937b
agus_prayogi <agusyogi600@gmail.com>
2020-05-27 13:12:06 +0700

    ViewPager

--- app/src/main/java/id/kanesa/kanesaservice/MainActivity.kt
+++ app/src/main/java/id/kanesa/kanesaservice/MainActivity.kt
@@ -1,12 +1,44 @@
 package id.kanesa.kanesaservice
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.graphics.Typeface
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import id.kanesa.kanesaservice.adapter.BottomNavPagerAdapter
+import id.kanesa.kanesaservice.ui.home.HomeFragment
+import id.kanesa.kanesaservice.ui.pesanan.PesananFragment
+import id.kanesa.kanesaservice.ui.profile.ProfileFragment
+import io.github.inflationx.viewpump.ViewPumpContextWrapper
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    override fun attachBaseContext(newBase: Context) {
+        super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bnlv = bottom_navigation_bar
+        bnlv.setTypeface(Typeface.createFromAsset(assets, "fonts/SF-Medium.otf"))
+        val fragList = ArrayList<Fragment>()
+        fragList.add(HomeFragment())
+        fragList.add(PesananFragment())
+        fragList.add(ProfileFragment())
+        val pagerAdapter = BottomNavPagerAdapter(fragList, supportFragmentManager)
+        val viewPager = view_pager
+        viewPager.offscreenPageLimit = 3
+        viewPager.adapter = pagerAdapter
+
+        bnlv.setNavigationChangeListener { _, position ->
+            viewPager.setCurrentItem(
+                position,
+                true
+            )
+        }
     }
 }
---
+++ app/src/main/java/id/kanesa/kanesaservice/adapter/BottomNavPagerAdapter.kt
@@ -0,0 +1,22 @@
+package id.kanesa.kanesaservice.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class BottomNavPagerAdapter(
+    private val fragmentList: List<Fragment>,
+    fm: FragmentManager?
+) :
+    FragmentStatePagerAdapter(fm!!) {
+    override fun getItem(position: Int): Fragment {
+        return if (position >= 0 && position < fragmentList.size) {
+            fragmentList[position]
+        } else Fragment()
+    }
+
+    override fun getCount(): Int {
+        return fragmentList.size
+    }
+
+}
